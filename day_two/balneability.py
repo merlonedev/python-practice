9080140d4bfdd82f6ed869ef75f62b236092b84b
Rodrigo Merlone <alestori7@gmail.com>
2022-01-29 18:45:36 -0300

    balneabilidade

---
+++ day_two/balneability.py
@@ -0,0 +1,10 @@
+import os
+import csv
+
+os.chdir("day_two")
+
+with open("balneabilidade.csv") as file:
+    beach_status_reader = csv.reader(file, delimiter=",", quotechar='"')
+    header, *data = beach_status_reader
+
+print(data)
